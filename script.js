8313719922f751ea140b241ab7cc2317e54207f0
David A Hale <115053604+david-a-hale@users.noreply.github.com>
2023-04-22 10:12:54 -0400

    edit script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let color = 'black';
+let click = true;
 
 function populateGrid(size) {
     let gridContainer = document.querySelector(".grid-container");
@@ -19,27 +20,35 @@ function populateGrid(size) {
 populateGrid(16);
 
 function changeSize(input) {
-    if (input >= 2 || input <= 100) {
-        populateGrid(input);
-    } else {
-        console.log('too many squares');
-    }
+    if (click) {
+        if (input >= 2 || input <= 100) {
+            populateGrid(input);
+        } else {
+            console.log('too many squares');
+        };
+    };
 };
 
 function colorSquare() {
-    if (color === 'random') {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } else {
-        this.style.backgroundColor = color;
+    if (click) {
+        if (color === 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
     }
 };
 
 function changeColor(choice) {
     color = choice;
-}
+};
 
 function resetGrid() {
     let gridContainer = document.querySelector('.grid-container');
     let squares = gridContainer.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = 'white');
 };
+
+document.querySelector("body").addEventListener("click", () => {
+    click = !click;
+});
